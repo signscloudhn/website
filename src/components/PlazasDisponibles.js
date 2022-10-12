ac40021b994f6c669b24e7e9fdf96b73520c961c
isai <isaiorellana.dev@gmail.com>
2022-10-12 11:15:39 -0600

    💬 Plazas Cards
    Actualizacion de las plazas, los datos que mostraran y el boton de aplicar.

--- src/components/PlazasDisponibles.js
+++ src/components/PlazasDisponibles.js
@@ -1,28 +1,52 @@
-import { plazas } from "../data/plazas";
+import { plazas } from "../data/plazas"
+import Location from "./common/Location"
 import "@styles/components/PlazasDisponibles.scss"
 
 const PlazasDisponibles = () => {
   return (
-    <section className="plazas" >
-      <a name="plazas" ></a>
+    <section className="plazas">
+      <a name="plazas"></a>
       <h2>Plazas Disponibles</h2>
 
-      <div className="plazas__cards-container" >
+      <div className="plazas__cards-container">
         {plazas.map((p) => {
-          return (<article className="plaza-card" key={p.puesto + Math.random()} >
-            <div>
-              <h4>{p.puesto}</h4>
-              <p>- {p.empresa}</p>
-              <p>- {p.jornada}</p>
-              <p>- {p.lugar}</p>
-            </div>
-            <a>Aplicar ya</a>
-          </article>)
+          return (
+            <article className="plaza-card" key={p.puesto + Math.random()}>
+              <div>
+                <h4>{p.puesto}</h4>
+                <p
+                  className="categoria"
+                  style={
+                    p.categoria === "Programacion"
+                      ? {
+                        backgroundColor: "#33cfff",
+                        boxShadow: "2px 1px 5px #d353df",
+                      }
+                      : {
+                        backgroundColor: "#d353df",
+                        boxShadow: "2px 1px 5px #33cfff",
+                      }
+                  }
+                >
+                  {p.categoria}
+                </p>
+                <p>- Tecnologias que utilizamos:</p>
+                <div className="tecnologias">
+                  {p.tecnologias.map((t) => (
+                    <p>- {t}</p>
+                  ))}
+                </div>
+                <p>
+                  <Location /> {p.lugar}
+                </p>
+              </div>
+              <a href="/#contact">Aplicar</a>
+            </article>
+          )
         })}
-
       </div>
     </section>
-  );
+  )
 }
 
-export default PlazasDisponibles;
+export default PlazasDisponibles
--- src/data/plazas.js
+++ src/data/plazas.js
@@ -1,26 +1,56 @@
 export const plazas = [
   {
-    puesto: "Desarrollador Backend con Python y Django",
-    empresa: "SignsCloud",
-    jornada: "Jornada completa",
-    lugar: "San Pedro Sula, Cortes"
+    puesto: "Desarrollador Backend",
+    categoria: "Programacion",
+    lugar: "San Pedro Sula, Cortes",
+    tecnologias: [
+      "Python",
+      "Django Rest Framework",
+      "MySQL, MariaDB o Postgresql",
+      "Git",
+      "API REST",
+      "Docker y Docker-compose",
+    ],
   },
   {
-    puesto: "Graphic Designer",
-    empresa: "SignsCloud",
-    jornada: "Jornada completa",
-    lugar: "San Pedro Sula, Cortes"
+    puesto: "Desarrolador Frontend",
+    categoria: "Programacion",
+    lugar: "San Pedro Sula, Cortes",
+    tecnologias: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "TypeScript",
+      "React JS",
+      "Angular",
+      "Git",
+      "Formik",
+    ],
   },
   {
-    puesto: "Graphic Designer",
-    empresa: "SignsCloud",
-    jornada: "Jornada completa",
-    lugar: "San Pedro Sula, Cortes"
+    puesto: "Graphic Designer Junior",
+    categoria: "Diseño Grafico",
+    lugar: "San Pedro Sula, Cortes",
+    tecnologias: ["InDesign", "Photoshop", "Illustrator"],
   },
   {
-    puesto: "Graphic Designer",
-    empresa: "SignsCloud",
-    jornada: "Jornada completa",
-    lugar: "San Pedro Sula, Cortes"
-  }
+    puesto: "Graphic Designer Senior",
+    categoria: "Diseño Grafico",
+    lugar: "San Pedro Sula, Cortes",
+    tecnologias: [
+      "Photoshop",
+      "Illustrator",
+      "After Effects",
+      "Premiere",
+      "Audition",
+      "Cinema 4D o Blender",
+    ],
+  },
+
+  {
+    puesto: "Social Media Designer",
+    categoria: "Diseño Grafico",
+    lugar: "San Pedro Sula, Cortes",
+    tecnologias: ["Photoshop", "Illustrator", "After Effects", "Premiere"],
+  },
 ]
