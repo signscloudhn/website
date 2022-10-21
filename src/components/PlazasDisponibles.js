574e5ab32089d8cdb897d073506900364ae2e81a
isai orellana <iorellana@signscloud.com>
2022-10-21 14:12:02 -0600

    💫 Animaciones con gsap
    Animacones con gsap e intersection observer en las cards de las plazas y en la seccion de contacto, tambien el logo tiene una animacion al renderizarse el dom

--- src/components/PlazasDisponibles.js
+++ src/components/PlazasDisponibles.js
@@ -2,13 +2,41 @@ import { Link } from "react-scroll"
 import { plazas } from "../data/plazas"
 import Location from "./common/Location"
 import "@styles/components/PlazasDisponibles.scss"
+import { gsap } from "gsap"
+import { useEffect, useRef } from "react"
 
 const PlazasDisponibles = () => {
+  const cardsContainer = useRef()
+
+  const observer = new IntersectionObserver(
+    (e) => {
+      e.forEach((entry) => {
+        if (entry.isIntersecting) {
+          gsap.to(entry.target, { translateX: 0, opacity: 1, delay: 0.5, ease: "back.out(1.7)" })
+          observer.unobserve(entry.target)
+        }
+      })
+    },
+    {
+      threshold: 0,
+      rootMargin: "50px",
+    }
+  )
+
+  useEffect(() => {
+    const childrens = cardsContainer.current?.childNodes
+
+    for (let index = 0; index < childrens.length; index++) {
+      const element = childrens[index]
+      observer.observe(element)
+    }
+  }, [])
+
   return (
     <section className="plazas" id="plazas">
       <h2>Plazas Disponibles</h2>
 
-      <div className="plazas__cards-container">
+      <div className="plazas__cards-container" ref={cardsContainer}>
         {plazas.map((p) => {
           return (
             <article className="plaza-card" key={p.puesto + Math.random()}>
@@ -33,9 +61,7 @@ const PlazasDisponibles = () => {
                 <p>- Tecnologias que utilizamos:</p>
                 <div className="tecnologias">
                   {p.tecnologias.map((t) => (
-                    <p
-                      key={t + Math.random()}
-                    >- {t}</p>
+                    <p key={t + Math.random()}>- {t}</p>
                   ))}
                 </div>
                 <p>
